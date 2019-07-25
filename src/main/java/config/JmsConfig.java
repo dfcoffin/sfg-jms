602e5a7537b8f50b3582d9c56b45f8fe95527d01
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-07-25 16:30:55 -0400

    Adding jms message converter config

---
+++ src/main/java/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by Donald F. Coffin on 07/25/2019 at 16:24
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
